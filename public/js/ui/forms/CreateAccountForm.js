60e22fc1db0cb275107f66f5c59106a73cb2e45d
NazarovAn <naatonchrome@gmail.com>
2020-04-10 00:44:46 +0400

    createAccountForm. js done

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -3,7 +3,7 @@
  * создания нового счёта
  * Наследуется от AsyncForm
  * */
-class CreateAccountForm {
+class CreateAccountForm extends AsyncForm{
   /**
    * Создаёт счёт с помощью Account.create и закрывает
    * окно (в котором находится форма) в случае успеха,
@@ -11,6 +11,15 @@ class CreateAccountForm {
    * и сбрасывает форму
    * */
   onSubmit( options ) {
-
+    Account.create(options.data, (err, response) => {
+      if(err){
+        console.log(err);
+        return        
+      }
+      
+      this.element.reset();
+      App.getModal('createAccount').close();
+      App.update();
+    })
   }
 }
